ab88569f250257e6e7143a2b8ff0c6da38f7056d
wfjohn <44661058+wfjohn@users.noreply.github.com>
2023-02-24 23:25:59 -0800

    Update diceroll.py

--- diceroll.py
+++ diceroll.py
@@ -9,13 +9,13 @@ total = [0, 0, 0, 0, 0, 0]
 i = 0
 while i < DICE_ROLLS_REQUIRED:
     i += 1
-    roll = (random.randrange(1, 7))
+    roll = (random.randrange(1, 7))      ### unnecessary brackets around the random call
     total[roll-1] += 1
 
 i = -1
-while i < len(total) - 1:
+while i < len(total) - 1:                ### this is better implemented with a for loop using range() to set the length. Much cleaner and you don't need a counter ... for loops give you that.
     i += 1
-    if total[i] == 1:
+    if total[i] == 1:                    ### good adjustment for output
         amount = "time"
     else:
         amount = "times"
